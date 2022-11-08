12d9705844a326945d77e156ff617eb3d6abebf6
codingpalette <codingpalette@gmail.com>
2022-11-08 20:03:09 +0900

    스웨거 작업

--- back/src/test/dto/test-post.dto.ts
+++ back/src/test/dto/test-post.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class testPostDto {
   @IsNumber()
+  @ApiProperty({
+    example: 'zerohch0@gmail.com',
+    description: '이메일',
+  })
   readonly id: number;
 
   @IsString()
+  @ApiProperty({
+    example: 'zerohch0@gmail.com',
+    description: '이메일',
+  })
   readonly username: string;
 }
--- back/src/test/test.controller.ts
+++ back/src/test/test.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Post, Body, Req, Res, HttpException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, Res, HttpException, Param } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiCookieAuth, ApiCreatedResponse, ApiQuery } from '@nestjs/swagger';
 import {testPostDto} from "./dto/test-post.dto";
 
+@ApiTags('Swagger') // swagger에 tag를 생성해줌
 @Controller('test')
 export class TestController {
 
-  @Get()
-  testGet() {
+  @ApiOperation({ summary: '워크스페이스 채널 모두 가져오기' })
+  @Get(':url/channels')
+  testGet(@Param('url') url,) {
     // throw new HttpException('제목을 입력해주세요', 401);
     return Object.assign({
       "aa":"안녕!"
     });
   }
 
+  @ApiOperation({ summary: '유저 생성 API', description: '유저를 생성한다.' })
   @Post()
   testPost(@Body() body: testPostDto) {
     console.log(body)
