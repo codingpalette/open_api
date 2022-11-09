9687a5d16d99bd3de8057eefcd919fe740b87ec0
codingpalette <codingpalette@gmail.com>
2022-11-09 16:24:38 +0900

    nest 핫 리로드 적용

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -32,5 +34,10 @@ async function bootstrap() {
     })
   );
   await app.listen(4000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
